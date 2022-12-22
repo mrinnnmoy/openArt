8b5a7cfbb80e699f9398275336da9112dcfbc901
mrinnnmoy <mrinmoy202000@gmail.com>
2022-12-22 19:11:27 +0530

    build header-component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,102 @@
+import React, { useRef, useEffect } from 'react';
+import "./Header.css";
+import { Container } from "reactstrap";
+
+import { NavLink, Link } from "react-router-dom";
+
+const Header = () => {
+    const NAV__LINKS = [
+        {
+            index: 1,
+            display: "Home",
+            url: "/home",
+        },
+        {
+            index: 2,
+            display: "Market",
+            url: "/market",
+        },
+        {
+            index: 3,
+            display: "Create",
+            url: "/create",
+        },
+        {
+            index: 1,
+            display: "Contact",
+            url: "/contact",
+        },
+    ];
+
+    const headerRef = useRef(null);
+
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (
+                document.body.scrollTop > 80 ||
+                document.documentElement.scrollTop > 80
+            ) {
+                headerRef.current.classList.add("header__shrink");
+            } else {
+                headerRef.current.classList.remove("header__shrink");
+            }
+        });
+
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
+
+    const toggleMenu = () => menuRef.current.classList.toggle("active__menu");
+
+    return (
+        <header className="header" ref={headerRef}>
+            <Container>
+                <div className="navigation">
+                    <div className="logo">
+                        <h2 className="d-flex gap-2 align-items-center">
+                            <span>
+                                <i className="ri-fire-fill"></i>
+                            </span>
+                            OpenArt
+                        </h2>
+                    </div>
+
+                    <div className="nav__menu" ref={menuRef} onClick={toggleMenu}>
+                        <ul className="nav__list">
+                            {NAV__LINKS.map((item, index) => (
+                                <li className="nav__item" key={index}>
+                                    <NavLink
+                                        to={item.url}
+                                        className={(navClass) =>
+                                            navClass.isActive ? "active" : ""
+                                        }
+                                    >
+                                        {item.display}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className="nav__right d-flex align-items-center gap-5">
+                        <button className="btn d-flex gap-2 align-items-center">
+                            <span>
+                                <i className="ri-wallet-line"></i>
+                            </span>
+                            <Link to="/wallet">Connect Wallet</Link>
+                        </button>
+
+                        <span className="mobile__menu">
+                            <i className="ri-menu-line" onClick={toggleMenu}></i>
+                        </span>
+                    </div>
+                </div>
+            </Container>
+        </header>
+    )
+};
+
+export default Header;
